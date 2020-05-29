c8033650312c716f7157d584ee83a452130352b0
amir00700 <43377462+amir00700@users.noreply.github.com>
2020-05-28 22:05:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
 <<<<<<< HEAD
-    res.end('Hello Professor');
+    res.end('Hello everybody');
 =======
-    res.end('Hello professor');
 >>>>>>> 9a5f747ba4da06837ce4c492aaaf2d6594a08832
 });
 
